57f98dcec24f8cd07eead4a1be5a86fff99f1468
Norio Suzuki <norisuzu@gmail.com>
2025-10-13 14:40:03 +0900

    Added a simple Veryl study code.

---
+++ TangNano9K/board_test/src/gowin_osc/gowin_osc_tmp.v
@@ -0,0 +1,17 @@
+//Copyright (C)2014-2025 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.12
+//Part Number: GW1NR-LV9QN88PC6/I5
+//Device: GW1NR-9
+//Device Version: C
+//Created Time: Mon Oct 13 13:04:17 2025
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_OSC your_instance_name(
+        .oscout(oscout) //output oscout
+    );
+
+//--------Copy end-------------------
